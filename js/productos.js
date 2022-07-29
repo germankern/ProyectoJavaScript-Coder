086ecbc8c23d18343d7a4df67741e4357ad40f40
Germán Kern <germankern890@gmail.com>
2022-07-28 23:09:29 -0300

    segunda entrega proyecto final

--- js/productos.js
+++ js/productos.js
@@ -1,112 +1,151 @@
 const productos = [
-    {
-        id: 1,
-        nombre: "Ryzen 7 5700g",
-        precio: 42000
-    },
-    {
-        id: 2,
-        nombre: "Ryzen 5 5600g",
-        precio: 34000
-    },
-    {
-        id: 3,
-        nombre: "Ryzen 7 5800x",
-        precio: 51000
-    },
-    {
-        id: 4,
-        nombre: "Ryzen 7 5600x",
-        precio: 35000
-    },
-    {
-        id: 5,
-        nombre: "Ryzen 9 5900x",
-        precio: 65000
-    }, 
-    {
-        id: 6,
-        nombre: "Intel i5 12400f",
-        precio: 27390
-    }, 
-    {
-        id: 7,
-        nombre: "Intel i5 12400",
-        precio: 30475
-    }, 
-    {
-        id: 8,
-        nombre: "Intel i7 12700kf",
-        precio: 60540
-    },
-    {
-        id: 9,
-        nombre: "Intel i7 12700k",
-        precio: 61980
-    },
-    {
-        id: 10,
-        nombre: "Intel i9 12900k",
-        precio: 97500
-    }, 
+  {
+    id: 1,
+    nombre: "Ryzen 7 5700g",
+    precio: 42000,
+    img: "../imagenes/ryzen-5000.png",
+  },
+  {
+    id: 2,
+    nombre: "Ryzen 5 5600g",
+    precio: 34000,
+    img: "../imagenes/ryzen-5000.png",
+  },
+  {
+    id: 3,
+    nombre: "Ryzen 7 5800x",
+    precio: 51000,
+    img: "../imagenes/ryzen-5000.png",
+  },
+  {
+    id: 4,
+    nombre: "Ryzen 7 5600x",
+    precio: 35000,
+    img: "../imagenes/ryzen-5000.png",
+  },
+  {
+    id: 5,
+    nombre: "Ryzen 9 5900x",
+    precio: 65000,
+    img: "../imagenes/ryzen-5000.png",
+  },
+  {
+    id: 6,
+    nombre: "Intel i5 12400f",
+    precio: 27390,
+    img: "../imagenes/intel-12.png",
+  },
+  {
+    id: 7,
+    nombre: "Intel i5 12400",
+    precio: 30475,
+    img: "../imagenes/intel-12.png",
+  },
+  {
+    id: 8,
+    nombre: "Intel i7 12700kf",
+    precio: 60540,
+    img: "../imagenes/intel-12.png",
+  },
+  {
+    id: 9,
+    nombre: "Intel i7 12700k",
+    precio: 61980,
+    img: "../imagenes/intel-12.png",
+  },
+  {
+    id: 10,
+    nombre: "Intel i9 12900k",
+    precio: 97500,
+    img: "../imagenes/intel-12.png",
+  },
 ];
 
-let aux = localStorage.getItem('productosEnCarro');
+// Guardar el LocalStorage: Hacer Local Stoe.setItem luego de JSONparse y stringify
+let aux = localStorage.getItem("productosEnCarro");
 let productosEnCarro;
 
 if (!aux) {
-    productosEnCarro = [];
+  productosEnCarro = [];
 } else {
-    productosEnCarro = JSON.parse(aux);
-    pintarProductosEnCarro();
+  productosEnCarro = JSON.parse(aux);
+  pintarProductosEnCarro();
 }
 
-
 function pintandoListado() {
-    let aux = '';
-    for(let i = 0; i < productos.length; i++){
-        
-        aux = aux + `<div style="margin: 85px; border: 5px solid; padding: 16px; font-size: 22px; text-align: center; background-color: lightblue">
-                        <h3>${productos[i].nombre}</h3>
-                        <p>$ ${productos[i].precio}</p>
-                        <button style="cursor: pointer; padding: 8px; margin-top: 22px; "
-                            onclick="agregarAlCarrito(
-                                {nombre: '${productos[i].nombre}', precio: ${productos[i].precio}})">Agregar al carro</button>
-                    </div>`;
-    }
-    // Ejemplo de DOM (acá lo "pintamos" en el DOM)
-    
-    document.getElementById('div-productos').innerHTML = aux;
+  let aux = "";
+  for (let i = 0; i < productos.length; i++) {
+    aux =
+      aux +
+      `<article>
+        <img class="procesadores-img" src=${productos[i].img} alt="microprocesador-amd-para-comprar"/>
+        <h3>${productos[i].nombre}</h3>
+        <p>4.2Ghz 8 Núcleos 16 Hilos</p>
+        <p><strong>$${productos[i].precio}</strong></p>
+        <p class="envio">Envío gratis</p>
+        <a
+          onclick="agregarAlCarrito({img: '${productos[i].img}', nombre: '${productos[i].nombre}', precio: '${productos[i].precio}'})"
+          id="but-carrito"
+          class="ver-mas"
+          href="#">🛒</a>
+      </article>`;
+  }
+  // Ejemplo de DOM (acá lo "pintamos" en el DOM)
+  document.getElementById("div-productos").innerHTML = aux;
 }
 pintandoListado();
 
-// ----- Funciones de Carro --------- //
+// ----- Funciones del Carrito --------- //
 function agregarAlCarrito(objetoProducto) {
-    productosEnCarro.push(objetoProducto);
+  productosEnCarro.push(objetoProducto);
 
-    localStorage.setItem('productosEnCarro', JSON.stringify(productosEnCarro));
-    pintarProductosEnCarro();
+  localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+  pintarProductosEnCarro();
 }
 
 function borrarDelCarro(indice) {
-    productosEnCarro.splice(indice, 1);
+  productosEnCarro.splice(indice, 1);
 
-    localStorage.setItem('productosEnCarro', JSON.stringify(productosEnCarro));
-    pintarProductosEnCarro();
+  localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+  pintarProductosEnCarro();
 }
+
 // ------------------------------- //
 
-
 function pintarProductosEnCarro() {
-    let aux = '';
-    for(let i=0; i < productosEnCarro.length; i++){
-        
-        aux = aux + `<div style="border: 4px solid green; display: flex; flex-direction: column; max-width: 200px; margin: 10px; text-align: center;">
+  let aux = "";
+  for (let i = 0; i < productosEnCarro.length; i++) {
+    aux =
+      aux +
+      `<div style="border: 3px solid green; display: flex; flex-direction: column; max-width: 120px; margin: 10px; text-align: center;">
+        <img class="procesadores-img" src=${productosEnCarro[i].img} alt="microprocesador-para-comprar"/>
                         <h3>${productosEnCarro[i].nombre}</h3>
                         <p>$ ${productosEnCarro[i].precio}</p>
-                        <button onclick="borrarDelCarro(${i})" style="cursor: pointer;">Eliminar 🗑️</button>
-                    </div>`;
-    }
-    // Ejemplo de DOM (acá lo "pintamos" en el DOM)
-    document.getElementById('div-carrito').innerHTML = aux;
+                        <button 
+                          onclick="borrarDelCarro(${i})" 
+                          style="cursor: pointer; color: white; background-color: red; padding: 3px;">Eliminar 🗑️
+                        </button>
+                    </div>
+                    `;
+  }
+  // Ejemplo de DOM (acá lo "pintamos" en el DOM)
+  document.getElementById("div-carrito").innerHTML = aux;
+  // Contador de Productos en Carrito
+  const contadorCarrito = document.getElementById("contadorCarrito");
+  contadorCarrito.innerText = productosEnCarro.length;
+  // Precio Total productos en carro
+  const precioTotal = document.getElementsByClassName("precioTotal");
+  precioTotal.innerText = productosEnCarro.reduce((acc, producto) => acc + producto.precio, 0);
 }
+
+const button = document.querySelector("button");
+const closebtn = document.querySelector(".closebtn");
+const nav = document.querySelector(".nav");
+
+button.onclick = () => {
+  nav.classList.add("show");
+};
+
+closebtn.onclick = () => {
+  nav.classList.remove("show");
+};
