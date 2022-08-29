6d55982d77dd610f3828ccdb941ac9fa90ec39a3
Germán Kern <germankern890@gmail.com>
2022-08-29 19:50:04 -0300

    haciendo funcional buscador

--- js/productos.js
+++ js/productos.js
@@ -1,27 +1,3 @@
-// class Producto {
-//   constructor(id, nombre, descripcion, precio, img, cantidad) {
-//     this.id = id;
-//     this.nombre = nombre;
-//     this.descripcion = descripcion;
-//     this.precio = precio;
-//     this.img = img;
-//     this.cantidad = cantidad;
-//   }
-// }
-
-// const productos = [
-//   new Producto(1, "Ryzen 5 5600g", "3.9Ghz 6 Núcleos 12 Hilos", 34000, "../imagenes/ryzen-5000.png", 1),
-//   new Producto(2, "Ryzen 5 5600x", "3.7Ghz 6 Núcleos 12 Hilos", 35000, "../imagenes/ryzen-5000.png", 1),
-//   new Producto(3, "Ryzen 7 5700g", "3.8Ghz 8 Núcleos 16 Hilos", 42000, "../imagenes/ryzen-5000.png", 1),
-//   new Producto(4, "Ryzen 7 5800x", "3.8Ghz 8 Núcleos 16 Hilos", 51000, "../imagenes/ryzen-5000.png", 1),
-//   new Producto(5, "Ryzen 9 5900x", "3.7Ghz 12 Núcleos 24 Hilos", 65000, "../imagenes/ryzen-5000.png", 1),
-//   new Producto(6, "Intel i5 12400f", "4.2Ghz 6 Núcleos 12 Hilos", 27390, "../imagenes/intel-12.png", 1),
-//   new Producto(7, "Intel i5 12400", "2.5Ghz 6 Núcleos 12 Hilos", 30475, "../imagenes/intel-12.png", 1),
-//   new Producto(8, "Intel i7 12700kf", "2.7Ghz 12 Núcleos 20 Hilos", 60540, "../imagenes/intel-12.png", 1),
-//   new Producto(9, "Intel i7 12700k", "4.2Ghz 12 Núcleos 20 Hilos", 61980, "../imagenes/intel-12.png", 1),
-//   new Producto(10, "Intel i9 12900k", "4.2Ghz 16 Núcleos 24 Hilos", 97500, "../imagenes/intel-12.png", 1),
-// ];
-
 // Guardar el LocalStorage: Hacer Local Stoe.setItem luego de JSONparse y stringify
 let productosEnCarro;
 
@@ -43,17 +19,17 @@ function pintandoListado() {
       let aux = "";
       for (let i = 0; i < productos.length; i++) {
         aux += `<article>
-        <img class="procesadores-img" src=${productos[i].img} alt="microprocesador-amd-para-comprar"/>
-        <h3>${productos[i].nombre}</h3>
-        <p>${productos[i].descripcion}</p>
-        <p><strong>$${productos[i].precio}</strong></p>
-        <p class="envio">Envío gratis</p>
-        <a
-          onclick="agregarAlCarrito({id: ${productos[i].id}, img: '${productos[i].img}', nombre: '${productos[i].nombre}', precio: ${productos[i].precio}, cantidad: ${productos[i].cantidad}})"
-          id="but-carrito"
-          class="ver-mas"
-          href="#">🛒</a>
-      </article>`;
+                  <img class="procesadores-img" src=${productos[i].img} alt="microprocesador-amd-para-comprar"/>
+                  <h3>${productos[i].nombre}</h3>
+                  <p>${productos[i].descripcion}</p>
+                  <p><strong>$${productos[i].precio}</strong></p>
+                  <p class="envio">Envío gratis</p>
+                  <a
+                    onclick="agregarAlCarrito({id: ${productos[i].id}, img: '${productos[i].img}', nombre: '${productos[i].nombre}', precio: ${productos[i].precio}, cantidad: ${productos[i].cantidad}})"
+                    id="but-carrito"
+                    class="ver-mas"
+                    href="#">🛒</a>
+                </article>`;
       }
       // Ejemplo de DOM (acá lo "pintamos" en el DOM)
       document.getElementById("div-productos").innerHTML = aux;
@@ -103,9 +79,12 @@ function agregarAlCarrito(objetoProducto) {
 // Borrar un item del carro
 function borrarDelCarro(indice) {
   productosEnCarro.splice(indice, 1);
-
   localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
   pintarProductosEnCarro();
+  // Cerrar carrito si no hay productos
+  if (productosEnCarro.length == 0) {
+    nav.classList.remove("show");
+  }
 }
 
 // Vaciar todo el carrito de compras
@@ -138,17 +117,18 @@ botonVaciar.addEventListener("click", () => {
 function pintarProductosEnCarro() {
   let aux = "";
   for (let i = 0; i < productosEnCarro.length; i++) {
-    aux += `<div style="border: 3px solid green; display: flex; flex-direction: column; max-width: 120px; margin: 10px; text-align: center;">
-            <img class="procesadores-img" src=${productosEnCarro[i].img} alt="microprocesador-para-comprar"/>
-                        <h3>${productosEnCarro[i].nombre}</h3>
-                        <p>$ ${productosEnCarro[i].precio}</p>
-                        <p>Cantidad: <span id="cantidad">${productosEnCarro[i].cantidad}</span></p>
-                        <button 
-                          onclick="borrarDelCarro(${i})" 
-                          style="cursor: pointer; color: white; background-color: red; padding: 3px;">Eliminar 🗑️
-                        </button>
-                    </div>
-                    `;
+    aux += `<div class="producto-en-carro">
+                <img class="procesadores-img" 
+                  src=${productosEnCarro[i].img} alt="microprocesador-para-comprar"/>
+                <h3>${productosEnCarro[i].nombre}</h3>
+                <p>$ ${productosEnCarro[i].precio}</p>
+                <p>Cantidad: <span id="cantidad">${productosEnCarro[i].cantidad}</span></p>
+                  <button class="borrar-producto"
+                    onclick="borrarDelCarro(${i})" 
+                    >Eliminar 🗑️
+                  </button>
+            </div>
+            `;
   }
   // Ejemplo de DOM (acá lo "pintamos" en el DOM)
   document.getElementById("div-carrito").innerHTML = aux;
@@ -183,3 +163,43 @@ button.onclick = () => {
 closebtn.onclick = () => {
   nav.classList.remove("show");
 };
+
+// Buscador
+let btnBuscar = document.getElementById("buscar-btn");
+
+btnBuscar.addEventListener("click", () => {
+  fetch("../data.json")
+    .then((respuestaInicial) => respuestaInicial.json())
+    .then((res) => {
+      let inputBuscar = document.getElementById("buscar").value;
+      console.log(res);
+      const produc = res.filter((p) => {
+        return p.nombre.toLowerCase().includes(inputBuscar.toLowerCase());
+      });
+
+      document.getElementById("div-productos").innerHTML = "";
+      if (produc.length != 0) {
+        for (let i = 0; i < produc.length; i++) {
+          document.getElementById("div-productos").innerHTML += `<article>
+                            <img class="procesadores-img" src=${produc[i].img} alt="microprocesador-amd-para-comprar"/>
+                            <h3>${produc[i].nombre}</h3>
+                            <p>${produc[i].descripcion}</p>
+                            <p><strong>$${produc[i].precio}</strong></p>
+                            <p class="envio">Envío gratis</p>
+                            <a
+                              onclick="agregarAlCarrito({id: ${produc[i].id}, img: '${produc[i].img}', nombre: '${produc[i].nombre}', precio: ${produc[i].precio}, cantidad: ${produc[i].cantidad}})"
+                              id="but-carrito"
+                              class="ver-mas"
+                              href="#">🛒</a>
+                          </article>`;
+          document.getElementById("textBusqueda").innerHTML = `Mostrando coincidencias con '${inputBuscar}'`;
+        }
+      } else {
+        document.getElementById("textBusqueda").innerHTML = `No se escontraron coincidencias con '${inputBuscar}'`;
+        pintandoListado();
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
